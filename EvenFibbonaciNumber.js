c74c1323066a23cbb3e9806131c5bacd990fe39d
Rhmatullah Darwish <rahmat.darwish@gmail.com>
2020-01-12 11:21:20 +0430

    Update EvenFibbonaciNumber.js

--- EvenFibbonaciNumber.js
+++ EvenFibbonaciNumber.js
@@ -4,17 +4,14 @@ function fiboEvenSum(number) {
   let sum = 0;
   
 
-for(var i=1; i<number; i++) {
+for(var i=0; i<=number; i++) {
   fibonacciSequence[i] = fibonacciSequence[i-2] ? (fibonacciSequence[i-2] + fibonacciSequence[i-1]) : 1;
-  if(fibonacciSequence[i] %2 === 0){
-    //fibonacciSequence.push(fibonacciSequence[i]);
-  }
+  
 }
 
   //return fibonacciSequence;
   for(var i=0; i<fibonacciSequence.length; i++){
     if(fibonacciSequence[i] %2 === 0){
-      //console.log('222')
       sum += fibonacciSequence[i];
     }
   }
